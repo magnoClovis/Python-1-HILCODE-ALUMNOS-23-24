2141c35119defe5676fe7e210eb53a7d117381a7
clovisHilcode <38765104+magnoClovis@users.noreply.github.com>
2023-10-07 21:54:45 +0200

    Create Clase 1 - print.py

---
+++ Clase 1 - print.py
@@ -0,0 +1,9 @@
+# Escribe tu código aquí :-)
+print("Hola\n")
+print("¿Qué tal?")
+
+nombre = input("¿Cómo te llamas?\n")
+print(nombre)
+apellidos = " Freitas dos Santos"
+print(apellidos)
+print("Te llamas:",nombre + apellidos,". Encantado")
